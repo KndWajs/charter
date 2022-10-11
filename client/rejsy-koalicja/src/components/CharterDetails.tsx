6a46c7a5ad40d5438c81ef1bb6a9267fb6974367
Konrad_Wajs <konrad.wajs@codelab.eu>
2022-10-11 21:39:49 +0200

    front-end minor improvements

--- client/rejsy-koalicja/src/components/CharterDetails.tsx
+++ client/rejsy-koalicja/src/components/CharterDetails.tsx
@@ -20,26 +20,32 @@ export default function CharterDetails({charter}: CharterDetailsProps) {
                 display: "flex",
                 justifyContent: "flex-start",
                 flexDirection: "column",
-                alignItems:"flex-start"
+                alignItems: "flex-start"
             }}
         >
-
             <p><b>Charter from</b> {formatDate(charter.from)} <b>to</b> {formatDate(charter.to)} </p>
-            <p><b>Total cost:</b> {charter.payoff?.totalCost}</p>
+            <p><b>Total cost: </b>
+                {Intl.NumberFormat('pl-PL', {style: 'currency', currency: 'PLN'})
+                    .format(charter.payoff!.totalCost!)}
+            </p>
             <p><b>Total discount:</b> {charter.payoff?.discountValue || 0}%</p>
             <p><b>Voucher code:</b> {charter.payoff?.voucher?.code}
-                {charter.payoff?.voucher?.code ? <React.Fragment> | discount: {charter.payoff?.voucher?.amount || 0}%</React.Fragment> : null}
+                {charter.payoff?.voucher?.code ?
+                    <React.Fragment> | discount: {charter.payoff?.voucher?.amount || 0}%</React.Fragment> : null}
             </p>
             <p><b>Discounts:</b>
                 {charter.payoff?.discounts?.map(function (discount, i) {
-                    return " "+ discount;
+                    return " " + discount;
                 })}
             </p>
             <p><b>Payments:</b>
                 {charter.payoff?.payments?.map(function (payment, i) {
                     return (
                         <div>
-                            <p>{i+1}. Payment date {formatDate(payment.paymentDate)} | Amount: {payment.amount}</p>
+                            <p>{i + 1}. Payment date: {formatDate(payment.paymentDate)} |
+                                Amount:
+                                {" " + Intl.NumberFormat('pl-PL', {style: 'currency', currency: 'PLN'})
+                                    .format(payment.amount)}</p>
                         </div>
                     )
                 })}
--- client/rejsy-koalicja/src/components/CharterForm.tsx
+++ client/rejsy-koalicja/src/components/CharterForm.tsx
@@ -2,7 +2,7 @@ import {Button, Callout, Classes,} from "@blueprintjs/core"
 import {DateRange, DateRangeInput2} from "@blueprintjs/datetime2"
 import moment from "moment";
 
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import CharterDto from "../shared/models/charterDto";
 import axios from "axios";
 import CharterDetails from "./CharterDetails";
@@ -12,8 +12,8 @@ export default function CharterForm() {
 
     const getEmptyCharter = (): CharterDto => {
         return {
-            from: new Date(),
-            to: new Date(),
+            from: new Date(2023, 7, 10),
+            to: new Date(2023, 7, 29),
             payoff: {
                 voucher: {code: ""}
             }
@@ -23,8 +23,7 @@ export default function CharterForm() {
     const [charter, setCharter] = useState(getEmptyCharter())
 
     // useEffect(() => {
-    //     setCharter()
-    // }, [])
+    // })
 
 
     const handleVoucherData = (e: React.FormEvent<HTMLInputElement>) => {
@@ -66,11 +65,8 @@ export default function CharterForm() {
         sendCharterDto(newCharter)
     }
 
-    function clear(clearFormular: boolean) {
-        if (clearFormular) {
-            setCharter(getEmptyCharter())
-        }
-        sendCharterDto({} as CharterDto)
+    function clear() {
+        setCharter(getEmptyCharter())
     }
 
     const handleKeypress = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -84,54 +80,76 @@ export default function CharterForm() {
         return moment(date).format("YYYY-MM-DD")
     }
 
+    let addSampleVoucher = () => {
+        setCharter({
+            ...charter,
+            payoff: {
+                ...charter.payoff,
+                voucher: {
+                    ...charter.payoff?.voucher,
+                    code: "SCD-001"
+                }
+            }
+        })
+    };
     return (
-        <div style={{width: 500}}>
+        <div style={{width: 600}}>
+
+            <h1>This Page is only for testing back-end!</h1>
+
             <Callout
                 style={{
                     marginBottom: "10px", display: "flex",
                     justifyContent: "flex-start",
-                    flexDirection: "column"
+                    flexDirection: "column",
+                    alignItems: "flex-start"
                 }}
 
             >
 
-                    <label>Voucher code:</label>
-                    <input
-                        id="code"
-                        value={charter?.payoff?.voucher?.code || ""}
-                        className={Classes.INPUT}
-                        placeholder="eg. SCD-001 or SCD-002"
-                        onChange={handleVoucherData}
-                        onKeyPress={handleKeypress}
+                <label>Voucher code:</label>
+                <div><input
+                    id="code"
+                    value={charter?.payoff?.voucher?.code || ""}
+                    className={Classes.INPUT}
+                    placeholder="eg. SCD-001 or SCD-002"
+                    onChange={handleVoucherData}
+                    onKeyPress={handleKeypress}
+                />
+                    <Button
+                        text="add sample voucher"
+                        onClick={addSampleVoucher}
                     />
+                </div>
+                <label>Charter: </label>
+                <DateRangeInput2
+                    formatDate={(date: any) => formatDate(date)}
+                    minDate={new Date()}
+                    maxDate={new Date(new Date().getFullYear() + 2, 12)}
+                    onChange={(range: DateRange) => {
 
-                    <label>Charter: </label>
-                    <DateRangeInput2
-                        formatDate={(date: any) => formatDate(date)}
-                        onChange={(range: DateRange) => {
-
-                            console.log(range[0])
-                            console.log(formatDate(range[0]))
-                            setCharter({
-                                ...charter,
-                                from: range[0] ? range[0] : new Date(),
-                                to: range[1] ? range[1] : new Date(),
-                            })
-                        }}
-                        parseDate={(str) => new Date(str)}
-                        shortcuts={false}
-                        value={[
-                            new Date(charter.from),
-                            new Date(charter.to),
-                        ]}
-                    />
+                        console.log(range[0])
+                        console.log(formatDate(range[0]))
+                        setCharter({
+                            ...charter,
+                            from: range[0] ? range[0] : new Date(2023, 7, 10),
+                            to: range[1] ? range[1] : new Date(2023, 7, 29),
+                        })
+                    }}
+                    parseDate={(str) => new Date(str)}
+                    shortcuts={false}
+                    value={[
+                        new Date(charter.from),
+                        new Date(charter.to),
+                    ]}
+                />
 
                 <div className="filtering-buttons">
                     <Button
                         minimal
                         text="Clear"
                         onClick={() => {
-                            clear(true)
+                            clear()
                         }}
                     />
                     <Button
